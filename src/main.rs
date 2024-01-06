93940f22b7d3349b071f6652298bfda089292a66
Similiukas <tautvaisassimas@gmail.com>
2024-01-06 19:58:20 +0200

    Add MiMC with external GF library

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use g2p::GaloisField;
 use crate::mimc::mimc::MiMC;
-use crate::utils::helpers::{add_field_elements_over_finite_field, generate_random_bits, multiply_finite_field, power_finite_field, to_decimal};
+use crate::mimc_lib::mimc_lib::{GF, MiMCLib};
+use crate::utils::helpers::{add_finite_field, generate_random_bits, multiply_finite_field, power_finite_field, to_decimal};
 
 mod utils;
 mod mimc;
+mod mimc_lib;
 
 fn test_addition() {
     let block_size = 5;
@@ -15,14 +18,12 @@ fn test_addition() {
     println!("{} {} {}\n\n", to_decimal(&a), to_decimal(&b), to_decimal(&c));
 
 
-    // println!();
-    // println!();
-    // println!();
-    //
-    // let c2 = add_field_elements_over_prime_field(&a, &b, 2_i32.pow(block_size) as u32, block_size);
-    //
-    // println!("{:?} {:?} {:?}", a, b, c2);
-    // println!("{} {} {}", to_decimal(&a), to_decimal(&b), to_decimal(&c2));
+    let a1: GF = (to_decimal(&generate_random_bits(block_size))).into();
+    let b1: GF = (to_decimal(&generate_random_bits(block_size))).into();
+
+    let c1 = a1 + b1;
+
+    println!("{:?} {:?} {:?}", a1, b1, c1);
 }
 
 fn test_mul() {
@@ -35,7 +36,16 @@ fn test_mul() {
     let d = power_finite_field(&c, 2);
 
     println!("{:?} {:?} {:?} {:?}", a, b, c, d);
-    println!("{} {} {} {}", to_decimal(&a), to_decimal(&b), to_decimal(&c), to_decimal(&d));
+    println!("{} {} {} {}\n", to_decimal(&a), to_decimal(&b), to_decimal(&c), to_decimal(&d));
+
+    let a1: GF = (to_decimal(&generate_random_bits(block_size))).into();
+    let b1: GF = (to_decimal(&generate_random_bits(block_size))).into();
+
+    let c1 = a1 * b1;
+
+    let d1 = c1.pow(21);
+    println!("{:?} {:?} {:?} {:?}", a1, b1, c1, d1);
+
 }
 
 fn test_mimc() {
@@ -48,10 +58,18 @@ fn test_mimc() {
     println!("Original:   {} {:?}", to_decimal(&plaintext), plaintext);
     let ciphertext = k.encrypt(&plaintext, &key);
     println!("Ciphertext: {} {:?}", to_decimal(&ciphertext), ciphertext);
-    let again = k.decrypt(&ciphertext);
-    println!("Decrypted:  {} {:?}", to_decimal(&again), again);
     let again = k.decrypt(&ciphertext, &key);
     println!("Decrypted:  {} {:?}\n\n", to_decimal(&again), again);
+
+
+    // let l = MiMCLib::new();
+    // println!("{l}");
+    // let plaintext: GF = (to_decimal(&generate_random_bits(17))).into();
+    // println!("Original:   {:?}", plaintext);
+    // let ciphertext = l.encrypt(&plaintext);
+    // println!("Ciphertext: {:?}", ciphertext);
+    // let p_again = l.decrypt(&ciphertext);
+    // println!("Decrypted:  {:?}", p_again);
 }
 
 fn main() {
---
+++ src/mimc_lib/mimc_lib.rs
@@ -0,0 +1,67 @@
+use std::fmt;
+use std::fmt::Formatter;
+use g2p::{g2p, GaloisField};
+use crate::utils::helpers::{generate_random_element, to_decimal};
+
+static BLOCK_SIZE: u32 = 17;
+
+g2p!(GF, 17);
+
+fn generate_round_constants(size: usize, field: u32) -> Vec<GF> {
+    let mut result: Vec<GF> = Vec::with_capacity(size);
+    result.push(GF::ZERO); // c_0 must be 0
+    for _ in 1..size {
+        result.push((to_decimal(&generate_random_element(field))).into());
+    }
+    result
+}
+
+pub struct MiMCLib {
+    rounds: usize,
+    field: u32,
+    round_constants: Vec<GF>
+}
+
+impl MiMCLib {
+    pub fn new() -> Self {
+        // For field 2 ** block_size it must be that block_size is odd
+        assert_eq!(BLOCK_SIZE % 2, 1, "Block size must be odd");
+        let rounds = (BLOCK_SIZE as f32 / 3f32.log(2.0)).ceil() as usize;
+        let field = 2u32.pow(BLOCK_SIZE);
+        MiMCLib {
+            rounds,
+            field,
+            round_constants: generate_round_constants(rounds, field)
+        }
+    }
+
+    pub fn encrypt(&self, plaintext: &GF) -> GF{
+        let mut state = plaintext.to_owned();
+        for &round in &self.round_constants {
+            state = (state + round).pow(3);
+        }
+        state
+    }
+
+    pub fn decrypt(&self, ciphertext: &GF) -> GF {
+        let mut state = *ciphertext;
+        let power = ((2_i32.pow(BLOCK_SIZE + 1) - 1) / 3) as usize;
+        for &round in self.round_constants[..1].iter().chain(self.round_constants[1..].iter().rev()) {
+            state = (state + round).pow(power);
+        }
+        state
+    }
+}
+
+impl fmt::Display for MiMCLib {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        // let converted_rc: &Vec<u32> = &self.round_constants.iter().map(|x| to_decimal(x)).collect();
+
+        f.debug_struct("MiMC using g2p library")
+            .field("\n  block size", &BLOCK_SIZE)
+            .field("\n  rounds", &self.rounds)
+            .field(&*format!("\n  field [2^{}]", BLOCK_SIZE), &self.field)
+            .field("\n  round constants", &self.round_constants)
+            .finish()
+    }
+}
